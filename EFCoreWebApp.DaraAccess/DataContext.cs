b14cc9482f6b73eed66144898ba6a42aaaa007c6
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-09 16:49:44 +0300

    Добавление контроллера пользователей

--- EFCoreWebApp.DaraAccess/DataContext.cs
+++ EFCoreWebApp.DaraAccess/DataContext.cs
@@ -7,14 +7,10 @@ namespace EFCoreWebApp.DaraAccess
     {
         public DbSet<User> Users { get; set; }
 
-        public DataContext()
+        public DataContext(DbContextOptions<DataContext> options) 
+            : base(options)
         {
             Database.EnsureCreated();
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
         }
     }
 }
