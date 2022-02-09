d82ba201c06467a37bc78ab1d3dd7ec48f54b44c
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-09 18:55:06 +0300

    Добавление класса инициализации БД

---
+++ EFCoreWebApp.DaraAccess/Data/TestData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using EFCoreWebApp.Core.Domain;
+
+namespace EFCoreWebApp.DaraAccess.Data
+{
+    public class TestData
+    {
+        public static IEnumerable<User> Users => new List<User>()
+        {
+            new User()
+                { Name = "Tom", Age = 26 },
+            new User()
+                { Name = "Jerry", Age = 12 },
+            new User()
+                { Name = "Bull", Age = 29 },
+        };
+    }
+}
