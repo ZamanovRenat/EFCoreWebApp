ad355c3e6641f050c926b72a22bdc1f96cc0a2ee
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-09 16:36:26 +0300

    Добавление класса контекста БД

---
+++ EFCoreWebApp.DaraAccess/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using EFCoreWebApp.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreWebApp.DaraAccess
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DataContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
