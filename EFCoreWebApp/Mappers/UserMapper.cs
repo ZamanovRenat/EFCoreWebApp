c8c08050fa23cd49492b6b9eede2796d68d30f76
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-09 20:33:30 +0300

    Класс для маппинга

---
+++ EFCoreWebApp/Mappers/UserMapper.cs
@@ -0,0 +1,23 @@
+﻿using EFCoreWebApp.Core.Domain;
+using EFCoreWebApp.Models;
+
+namespace EFCoreWebApp.Mappers
+{
+    public class UserMapper
+    {
+        public static User MapFromModel(UserViewModel model, User user)
+        {
+            if (user == null)
+            {
+                user = new User();
+            }
+
+            user.Id = model.Id;
+            user.Name = model.Name;
+            user.Age = model.Age;
+            user.Email = model.Email;
+
+            return user;
+        }
+    }
+}
