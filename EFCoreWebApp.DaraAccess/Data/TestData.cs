942792ae8ba3f27ba49f8ad50cef5c0fcad45003
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-10 11:10:15 +0300

    Добавление тестовых данных

---
+++ EFCoreWebApp.DaraAccess/Data/TestData.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EFCoreWebApp.Core.Domain;
+
+namespace EFCoreWebApp.DaraAccess.Data
+{
+    public class TestData
+    {
+        public static IEnumerable<User> Users => new List<User>()
+        {
+            new User()
+            {
+                Id = 1,
+                Name = "Tom",
+                Age = 23,
+                CompanyId = 1, 
+            },
+            new User()
+            {
+                Id = 2,
+                Name = "Bob",
+                Age = 23,
+                CompanyId = 2,
+            },
+            new User()
+            {
+                Id = 3,
+                Name = "Sam",
+                Age = 23,
+                CompanyId = 2,
+            },
+        };
+
+        public static IEnumerable<Company> Companies => new List<Company>()
+        {
+            new Company()
+            {
+                Id = 1,
+                Name = "Google"
+            },
+            new Company()
+            {
+                Id = 2,
+                Name = "Microsoft"
+            },
+        };
+    }
+}
--- EFCoreWebApp/Controllers/CompanyController.cs
+++ EFCoreWebApp/Controllers/CompanyController.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFCoreWebApp.Core.Domain;
+using EFCoreWebApp.DaraAccess;
+using Microsoft.Extensions.Logging;
 
 namespace EFCoreWebApp.Controllers
 {
@@ -11,5 +14,14 @@ namespace EFCoreWebApp.Controllers
     [ApiController]
     public class CompanyController : ControllerBase
     {
+        private readonly DataContext _dataContext;
+        private readonly ILogger<CompanyController> _logger;
+
+        public CompanyController(DataContext dataContext, ILogger<CompanyController> logger)
+        {
+            _dataContext = dataContext;
+            _logger = logger;
+            
+        }
     }
 }
