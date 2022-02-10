86bc4ff9dab5b4e6adb0f8dad448c204312bff09
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-10 11:52:33 +0300

    Репозиторий БД

---
+++ EFCoreWebApp.DaraAccess/Repositories/EfRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EFCoreWebApp.Core.Abstractions.Repositories;
+using EFCoreWebApp.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreWebApp.DaraAccess.Repositories
+{
+    public class EfRepository<T> : IRepository<T>
+        where T : BaseEntity
+    {
+        private readonly DataContext _dataContext;
+
+        public EfRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var entities = await _dataContext.Set<T>().ToListAsync();
+
+            return entities;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await _dataContext.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+
+            return entity;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dataContext.Set<T>().AddAsync(entity);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dataContext.Set<T>().Remove(entity);
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
