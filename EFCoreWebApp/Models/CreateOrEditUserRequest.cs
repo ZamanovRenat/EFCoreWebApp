23bdf998eb0490695d9c0d215f147adeb8865606
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-11 18:56:43 +0300

    Добавление вспомагетльной вью модели для скрытия Id при редактировании и создании

---
+++ EFCoreWebApp/Models/CreateOrEditUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreWebApp.Models
+{
+    public class CreateOrEditUserRequest
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
