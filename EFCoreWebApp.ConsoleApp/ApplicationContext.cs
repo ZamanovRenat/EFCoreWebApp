92a048e0255b6d8b6f43e64aa7a4ceb2af5f1ce2
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-14 16:37:16 +0300

    Добавления класса контекста БД

---
+++ EFCoreWebApp.ConsoleApp/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using EFCoreWebApp.ConsoleApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreWebApp.ConsoleApp
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Company> Companies { get; set; } = null!;
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=consoleapp.db");
+        }
+    }
+}
