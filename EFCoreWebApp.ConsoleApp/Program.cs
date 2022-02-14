684c518ca953d6b24134ac678937b6331fe3f743
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-14 16:39:17 +0300

    Заполнение данных вывод на консоль

--- EFCoreWebApp.ConsoleApp/Program.cs
+++ EFCoreWebApp.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using EFCoreWebApp.ConsoleApp.Entities;
 
 namespace EFCoreWebApp.ConsoleApp
 {
@@ -6,7 +8,23 @@ namespace EFCoreWebApp.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Company company1 = new Company { Name = "Google" };
+                Company company2 = new Company { Name = "Microsoft" };
+                User user1 = new User { Name = "Tom", Company = company1 };
+                User user2 = new User { Name = "Bob", Company = company2 };
+                User user3 = new User { Name = "Sam", Company = company2 };
+
+                db.Companies.AddRange(company1, company2);  // добавление компаний
+                db.Users.AddRange(user1, user2, user3);     // добавление пользователей
+                db.SaveChanges();
+
+                foreach (var user in db.Users.ToList())
+                {
+                    Console.WriteLine($"{user.Name} работает в {user.Company?.Name}");
+                }
+            }
         }
     }
 }
